40b420d4da79499353c2c702e1d995463e88276f
Gaurav Lohia <grvlohia6@gmail.com>
2020-07-20 20:24:04 +0530

    DOM methods lesson

---
+++ 13-DOM/03_important_selector_methods/script.js
@@ -0,0 +1,6 @@
+// The document comes with a bunch of methods for selecting elements. We're going to learn about the following 5:
+// document.getElementById()
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelector(): Returns the first element that matches a given CSS-style selector
+// document.querySelectorAll(): Returns a list of elements mathcing the given CSS-style selector
