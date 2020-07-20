0c1b90c45022733c4f332f0c9dd417feae7fd690
Gaurav Lohia <grvlohia6@gmail.com>
2020-07-20 09:01:16 +0530

    array iteration lesson

---
+++ 11-JavaScript-Basics/04_arrays/04-array-iteratioin/script.js
@@ -0,0 +1,11 @@
+var colors = ["red", "green", "yellow", "black", "orange"];
+
+for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+console.log("*****************************");
+
+colors.forEach(function(color) {
+    console.log(color);
+});
