f668b419a8f89204d5cce08fcdcabb6d1bfb41bc
Gaurav Lohia <grvlohia6@gmail.com>
2020-07-21 14:34:30 +0530

    .

--- 13-DOM/07_manipulating_attributes/script.js
+++ 13-DOM/07_manipulating_attributes/script.js
@@ -2,7 +2,7 @@
 
 var img = document.getElementsByTagName("img")[0];
 
-img.setAt0tribute("src", "https://i.pinimg.com/originals/57/38/33/57383338a6056c30eaaa83db693ba7cc.jpg"); 
+img.setAttribute("src", "https://i.pinimg.com/originals/57/38/33/57383338a6056c30eaaa83db693ba7cc.jpg"); 
 
 var google_link = document.querySelector("a");
 
