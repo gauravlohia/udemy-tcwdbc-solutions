501a686988468a614c4bb8ce09241e07a959ba79
Gaurav Lohia <grvlohia6@gmail.com>
2020-08-03 01:03:30 +0530

    completed todo list project

--- 18-todo-list-project/assets/js/todos.js
+++ 18-todo-list-project/assets/js/todos.js
@@ -1,1 +1,27 @@
-alert("Connected");
+// Check off specific todos by clicking
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+// Click on X to delete todo
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    })
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        // grabbing new todo text from input
+        var newTodo = $(this).val();
+        // create a new li and add to ul
+        var list = $("ul");
+        list.append("<li><span><i class='fa fa-trash'></i></span> " + newTodo + "</li>");
+        $(this).val("");
+    }
+});
+
+$(".fa-plus").click(function() {
+    $("input[type='text']").fadeToggle();
+});
