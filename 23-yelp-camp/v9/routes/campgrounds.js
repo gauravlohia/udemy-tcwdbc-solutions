a3611bc4a902aac7bf6b90b9c26f55115fe59873
Gaurav Lohia <grvlohia6@gmail.com>
2020-08-23 14:01:34 +0530

    Campground and User association

--- 23-yelp-camp/v9/routes/campgrounds.js
+++ 23-yelp-camp/v9/routes/campgrounds.js
@@ -22,12 +22,17 @@ router.post('/', isLoggedIn, (req, res) => {
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = { name: name, image: image, description: desc };
+    var author  = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = { name: name, image: image, description: desc, author: author };
     // Create a new campground and save to DB
     Campground.create(newCampground, (err, newlyCreated) => {
         if (err) {
             console.log(err);
         } else {
+            console.log(newlyCreated);
             res.redirect('/campgrounds');
         }
     });
