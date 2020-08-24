a3c78a16081752c10df7bd2c7c875e61db6342eb
Gaurav Lohia <grvlohia6@gmail.com>
2020-08-24 22:46:04 +0530

    added v11 for YelpCamp UI and cleaning Up

---
+++ 23-yelp-camp/v11/routes/index.js
@@ -0,0 +1,53 @@
+var express = require('express'),
+    router = express.Router(),
+    User = require("../models/user"),
+    passport = require('passport');
+
+// root route
+router.get('/', (req, res) => {
+    // console.log(res.locals);
+    res.render('landing')
+});
+
+
+// ============================================================================
+// AUTH ROUTES
+// ============================================================================
+router.get("/register", (req, res) => {
+    res.render("register");
+});
+
+//handle sign up logic
+router.post("/register", (req, res) => {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, (err, user) => {
+        if(err) {
+            console.log(err);
+            return res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/campgrounds");
+            });
+        }
+    });
+});
+
+// show login form
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+// handle login logic
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), (req, res) => {
+});
+
+// logout route
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+module.exports = router;
